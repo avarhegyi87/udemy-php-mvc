ae466b4f2b5a91124d887aa9c74a02e585c94f34
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2022-12-05 01:30:14 +0100

    basic routing in index.php and Router.php

--- Core/Router.php
+++ Core/Router.php
@@ -1,11 +1,20 @@
 <?php
 
+/**
+ *
+ */
 class Router {
 	/**
 	 * Associative array of routes (the routing table)
 	 * @var array
 	 */
-	protected $routes = [];
+	protected array $routes = [];
+
+	/**
+	 * Parameters from the matched route
+	 * @var array
+	 */
+	protected array $params = [];
 
 	/**
 	 * Add a route to the routing table
@@ -14,7 +23,7 @@ class Router {
 	 * @param array $params		Parameters (controller, action, etc.)
 	 * @return void
 	 */
-	public function add($route, $params) {
+	public function add(string $route, array $params) {
 		$this->routes[$route] = $params;
 	}
 
@@ -26,6 +35,32 @@ class Router {
 	public function getRoutes(): array {
 		return $this->routes;
 	}
+
+	/**
+	 * Match the route to the routes in the routing table, setting the $params property if a route is found
+	 *
+	 * @param string $url	The route URL
+	 * @return bool			true if a match is found, false otherwise
+	 */
+	public function match(string $url): bool {
+		foreach ($this->routes as $route => $params) {
+			if ($url == $route) {
+				$this->params = $params;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Get the currently matched parameters
+	 *
+	 * @return array
+	 */
+	public function getParams(): array {
+		return $this->params;
+	}
+
 
 
 }
--- public/index.php
+++ public/index.php
@@ -1,7 +1,23 @@
 <?php
 
+/* Front controller */
+
 require "../Core/Router.php";
 
 $router = new Router();
 
-echo get_class($router);
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+	echo '<pre>';
+	var_dump($router->getParams());
+	echo '</pre>';
+} else {
+	echo "No route found for this URL: $url";
+}
