3200e2b2d3902276355678a7afd802ad39dda501
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2022-12-14 00:20:01 +0100

    dynamic routing to controllers

--- Core/Router.php
+++ Core/Router.php
@@ -23,7 +23,19 @@ class Router {
 	 * @param array $params		Parameters (controller, action, etc.)
 	 * @return void
 	 */
-	public function add(string $route, array $params) {
+	public function add(string $route, array $params = []) {
+		// Convert the line into a regular expression: escape forward slashes
+		$route = preg_replace('/\//', '\\/', $route);
+
+		// Convert variables, e.g. {controller}
+		$route = preg_replace('/{([a-z-]+)}/', '(?P<\1>[a-z-]+)', $route);
+
+		// Convert variables with custom regular expressions e.g. {id:\d+}
+		$route = preg_replace('/{([a-z-]+):([^}]+)}/', '(?P<\1>\2)', $route);
+
+		// Add start and end delimiters, and case-insensitive flag (i)
+		$route = '/^' . $route . '$/i';
+
 		$this->routes[$route] = $params;
 	}
 
@@ -43,13 +55,69 @@ class Router {
 	 * @return bool			true if a match is found, false otherwise
 	 */
 	public function match(string $url): bool {
-		foreach ($this->routes as $route => $params) {
-			if ($url == $route) {
+
+		foreach ($this->routes as $route => $params){
+			if (preg_match($route, $url, $matches)) {
+				foreach ($matches as $key => $match) {
+					if (is_string($key)) {
+						$params[$key] = $match;
+					}
+				}
+
 				$this->params = $params;
 				return true;
 			}
 		}
+
 		return false;
+	}
+
+	/**
+	 * Dispatch the route, creating the controller object and running the action method
+	 *
+	 * @param string $url	The route URL
+	 * @return void
+	 */
+	public function dispatch(string $url) {
+		if ($this->match($url)) {
+			$controller = $this->convertToStudlyCaps($this->params['controller']);
+
+			if (class_exists($controller)) {
+				$controller_object = new $controller();
+
+				$action = $this->convertToCamelCase($this->params['action']);
+
+				if (is_callable([$controller_object, $action])) {
+					$controller_object->$action();
+				} else {
+					echo "Method $action (in controller $controller) not found.";
+				}
+			} else {
+				echo "Controller class $controller not found.";
+			}
+		} else {
+			echo "No route matched.";
+		}
+	}
+
+	/**
+	 * Convert the string with hyphens to StudlyCaps, e.g. post-authors => PostAuthors
+	 *
+	 * @param string $string The string to convert
+	 * @return string
+	 */
+	protected function convertToStudlyCaps(string $string): string {
+		return str_replace(' ', '', ucwords(str_replace('-', '', $string)));
+	}
+
+	/**
+	 * Convert the string with hyphens to camelCase, e.g. add-new => addNew
+	 *
+	 * @param $string
+	 * @return string
+	 */
+	protected function convertToCamelCase($string): string {
+		return lcfirst($this->convertToStudlyCaps($string));
 	}
 
 	/**
--- public/index.php
+++ public/index.php
@@ -2,6 +2,10 @@
 
 /* Front controller */
 
+// Require the controller class
+require '../App/Controllers/Posts.php';
+
+// Routing
 require "../Core/Router.php";
 
 $router = new Router();
@@ -9,15 +13,25 @@ $router = new Router();
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('{controller}/{id:\d+}/{action}');
+$router->add('admin/{action}/{controller}');
+
+/*// Display the routing table
+echo '<pre>';
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '</pre>';
 
 // Match the requested route
 $url = $_SERVER['QUERY_STRING'];
+
 
 if ($router->match($url)) {
 	echo '<pre>';
 	var_dump($router->getParams());
 	echo '</pre>';
 } else {
-	echo "No route found for this URL: $url";
-}
+	echo "No route found for URL '$url'";
+}*/
+
+$router->dispatch($_SERVER['QUERY_STRING']);
